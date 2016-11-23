5e6d6508e2c0c09298299ad683f4c919c12cd9d6
Fawaz Farid <fawwaz.ali@andela.com>
2016-11-23 21:22:15 +0300

    [Feature #133842277] fixed  pause tests

--- hc/api/tests/test_pause.py
+++ hc/api/tests/test_pause.py
@@ -13,6 +13,8 @@ class PauseTestCase(BaseTestCase):
                              HTTP_X_API_KEY="abc")
 
         ### Assert the expected status code and check's status
+        self.assertEquals(r.status_code, 200)
+        self.assertEquals(check.status, "up")
 
     def test_it_validates_ownership(self):
         check = Check(user=self.bob, status="up")
@@ -25,3 +27,13 @@ class PauseTestCase(BaseTestCase):
         self.assertEqual(r.status_code, 400)
 
         ### Test that it only allows post requests
+
+    def test_only_allows_post_requests(self):
+        # random generated UUID
+        check_code = '04dd700e-21f5-4e1b-a7ae-feb8721b708b'
+        url = "/api/v1/checks/%s/pause" % check_code
+
+        # Trying GET request
+        r = self.client.get(url, HTTP_X_API_KEY="abc")
+
+        self.assertEqual(r.status_code, 405)
