c843a47e01d9b41ec14e73628ddc1a32373ca009
Marvin_Kangethe <marvin.kangethe@andela.com>
2016-11-24 12:04:18 +0300

    [Feature #133842279] Add implementation on the test cases

--- hc/accounts/tests/test_switch_team.py
+++ hc/accounts/tests/test_switch_team.py
@@ -14,6 +14,11 @@ class SwitchTeamTestCase(BaseTestCase):
         r = self.client.get(url, follow=True)
 
         ### Assert the contents of r
+        """
+        Since r contains the url and this url has the name of the alice.
+        then name of alice must be in the url
+        """
+        self.assertContains(r,"This belongs to Alice")
 
 
     def test_it_checks_team_membership(self):
@@ -22,6 +27,14 @@ class SwitchTeamTestCase(BaseTestCase):
         url = "/accounts/switch_team/%s/" % self.alice.username
         r = self.client.get(url)
         ### Assert the expected error code
+        """
+        The url will raise a 403 error because the team leader {alice}
+        has not been set. 
+        Forbidden error, The client cannot access that url  because 
+        charlie is not in alice team.
+        """
+
+        self.assertEqual(r.status_code, 403)
 
     def test_it_switches_to_own_team(self):
         self.client.login(username="alice@example.org", password="password")
@@ -29,3 +42,8 @@ class SwitchTeamTestCase(BaseTestCase):
         url = "/accounts/switch_team/%s/" % self.alice.username
         r = self.client.get(url, follow=True)
         ### Assert the expected error code
+        """
+        it switches to the team created by alice.
+        The code is 200 mean ok, the server answered the request successfully.
+        """
+        self.assertEqual(r.status_code, 200)
