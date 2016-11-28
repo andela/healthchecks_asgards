163aad180ff39885b7a53d2e046c9b07affd8e4f
andela-akhenda <joseph.akhenda@andela.com>
2016-11-28 03:27:39 +0300

    [Feature #133842277] Updated test to set Alice to be staff and superuser and then save her

--- hc/api/tests/test_admin.py
+++ hc/api/tests/test_admin.py
@@ -7,8 +7,10 @@ class ApiAdminTestCase(BaseTestCase):
     def setUp(self):
         super(ApiAdminTestCase, self).setUp()
         self.check = Check.objects.create(user=self.alice, tags="foo bar")
-
         ### Set Alice to be staff and superuser and save her :)
+        self.alice.is_staff = True
+        self.alice.is_superuser = True
+        self.alice.save()
 
     def test_it_shows_channel_list_with_pushbullet(self):
         self.client.login(username="alice@example.org", password="password")
