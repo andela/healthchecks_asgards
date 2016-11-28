4b18328bb629ab21ec9f1f99d7b9bb8bfdeb83a4
andela-akhenda <joseph.akhenda@andela.com>
2016-11-28 03:44:52 +0300

    [Feature #133842277] Updated test to assert the expected response status code

--- hc/api/tests/test_badge.py
+++ hc/api/tests/test_badge.py
@@ -14,6 +14,7 @@ class BadgeTestCase(BaseTestCase):
     def test_it_rejects_bad_signature(self):
         r = self.client.get("/badge/%s/12345678/foo.svg" % self.alice.username)
         ### Assert the expected response status code
+        self.assertEqual(r.status_code, 400)
 
     def test_it_returns_svg(self):
         sig = base64_hmac(str(self.alice.username), "foo", settings.SECRET_KEY)
