8be2160be4d6e17ee273ab40312c9d55b0c225a7
Toni Solarin-Sodara <toni.edward@outlook.com>
2016-12-14 10:56:01 +0100

    Fix email login link

--- hc/settings.py
+++ hc/settings.py
@@ -127,7 +127,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-SITE_ROOT = "http://localhost:8000"
+SITE_ROOT = os.environ.get('SITE_ROOT') or "http://localhost:8000"
 PING_ENDPOINT = SITE_ROOT + "/ping/"
 PING_EMAIL_DOMAIN = HOST
 STATIC_URL = '/static/'
